7ae3bd9554e4b3f78fe9dbe275d7f4d263b40826
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-05 09:11:19 +0200

    commit1

---
+++ lesson3/task4/src/Greeting.jsx
@@ -0,0 +1,22 @@
+import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const rootElement = document.querySelector("#root");
+/*function get_age(date) {
+   let date1 = new Date(date);
+   let age = date1.getTime();
+   let age2 = new Date().getTime();
+   let age3 = Math.round((age2 - age)/ (24 * 3600 * 365 * 1000));
+   return age3;
+}*/
+    
+const Greeting = (props)=>{
+    let age = new Date(props.birthDate);
+   let age1 = age.getTime();
+   let age2 = new Date().getTime();
+   let age3 = Math.round((age2 - age1)/ (24 * 3600 * 365 * 1000));
+    return (
+        <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${age3} years old`}</div>
+    )
+}
+ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate="Jan, 15, 85 20:17:40 GMT+00:00"/>, rootElement);
