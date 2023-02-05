bacbaf84113991fbd42a0cc6192f5d7be7a32dcf
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-05 10:21:49 +0200

    commit1

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -4,17 +4,17 @@ import ReactDOM from 'react-dom';
 const rootElement = document.querySelector("#root");
 
     
-const Greeting = (props)=>{
+function Greeting(props) {
     const difer = new Date() - new Date(props.birthDate);
-  const yearsOld = Math.trunc(difer / 31536000000);
-
+    const yearsOld = Math.trunc(difer / 31536000000);
+  
     return (
-       
-        <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${yearsOld} years old`}</div>
-     
-    )
-    
-}
-export default Greeting;
+      <div className="greeting">
+        {`My name is ${props.firstName} ${props.lastName}. I am ${yearsOld} years old`}
+      </div>
+    );
+  }
+  
+  export default Greeting;
 
 ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate="Feb, 6, 85 20:17:40 GMT+00:00"/>, rootElement);
