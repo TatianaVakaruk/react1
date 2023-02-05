032a6832bf547b179218a664bef7febab9fe97ee
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-05 21:12:21 +0200

    commit1

--- lesson3/task3/src/Comment.jsx
+++ lesson3/task3/src/Comment.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './comment.scss';
 import './index.scss';
 import moment from 'moment';
 import UserInfo1 from './UserInfo.jsx';
 
 const formatDate = date => moment(date).format('DD MMM YYYY');
-const UserInfo = {
-  name: 'Tom',
-  avatarUrl: 'https://avatars1.githubusercontent.com/u10001'
-};
+
 function Comment(props) {
   return (
     <div className="comment">
@@ -19,14 +15,4 @@ function Comment(props) {
     </div>
   );
 }
-
-const rootElement = document.querySelector("#root");
-
-ReactDOM.render(
-     <Comment 
-    author={UserInfo}
-    text="Good job"
-    data={new Date('2012-04-23T18:25:43.511Z')} 
-  />,
-    rootElement
-);
+export default Comment;
