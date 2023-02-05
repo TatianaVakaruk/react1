f69264152753cbde1fffa1b9edb54c0418379883
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-05 11:32:44 +0200

    commit1

--- lesson3/task42/src/Greeting.jsx
+++ lesson3/task42/src/Greeting.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-function Greeting(props) {
-    const difer = new Date() - new Date(props.birthDate);
-    const yearsOld = Math.trunc(difer / 31536000000);
+const Greeting = (props)=>{
+  let age = new Date(props.birthDate);
+ let age1 = age.getTime();
+ let age2 = new Date().getTime();
+ let age3 = Math.abs(Math.floor((age2 - age1)/ (24 * 3600 * 365 * 1000)));
+  return (
+     
+      <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${age3} years old`}</div>
+   
+  )
   
-    return (
-      <div className="greeting">
-        {`My name is ${props.firstName} ${props.lastName}. I am ${yearsOld} years old`}
-      </div>
-    );
-  }
+}
 export default Greeting;
