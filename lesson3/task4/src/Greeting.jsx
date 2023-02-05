2556a29d62a96eb3d826d56362ebe6885b087294
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-05 09:45:48 +0200

    commit1

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -8,7 +8,7 @@ const Greeting = (props)=>{
     let age = new Date(props.birthDate);
    let age1 = age.getTime();
    let age2 = new Date().getTime();
-   let age3 = Math.floor((age2 - age1)/ (24 * 3600 * 365 * 1000));
+   let age3 = Math.abs(Math.floor((age2 - age1)/ (24 * 3600 * 365 * 1000)));
     return (
        
         <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I am ${age3} years old`}</div>
