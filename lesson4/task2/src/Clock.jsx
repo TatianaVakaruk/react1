8394178bd7a0481e73fa5b9b6943eba95a92b098
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-08 17:48:04 +0200

    сщььше1

--- lesson4/task2/src/Clock.jsx
+++ lesson4/task2/src/Clock.jsx
@@ -1,60 +1,23 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
-const getTimeWithOffset = offset => {
+const getTimeWithOffset = offset=> {
     const currentTime = new Date();
     const utcOffset = currentTime.getTimezoneOffset() / 60;
-let clock1 =new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
-
-  let hours = clock1.getHours();
- let  minutes= clock1.getMinutes();
-  let seconds = clock1.getSeconds();
-  
-   let clock;
-  if (hours > 12){
-    if (seconds < 10){
-      seconds = "0" + seconds;
-       }
-      if (minutes < 10){
-        minutes = "0" + minutes;
-       
-      }
-    hours = hours - 12;
-clock = `${hours}:${minutes}:${seconds} P.M.`;
-  }
-  
-  else if (hours === 24){
-    if (seconds < 10){
-        seconds = "0" + seconds;
-       }
-      if (minutes < 10){
-        minutes = "0" + minutes;
-       
-      }
-    clock = `00: ${minutes}:${seconds} A.M.`;
-  }
-  else{
-    if (seconds < 10){
-        seconds = "0" + seconds;
-       }
-      if (minutes < 10){
-        minutes = "0" + minutes;
-       
-      }
-    clock = `${hours}: ${minutes}:${seconds} A.M.`;
-  }
-  return clock;
-  }; 
+return moment(new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset))).format('h:mm:ss');
+ }; 
 
 class Clock extends Component {
     constructor(props){
         super(props);
         this.state={
             getTimeWithOffset:getTimeWithOffset(props.offset)
-        }
+}
         
 setInterval(()=>{
     this.setState({
         getTimeWithOffset:getTimeWithOffset(props.offset)
+        
     });
     
     }, 1000);
@@ -63,7 +26,10 @@ setInterval(()=>{
 
     render (){
         return (
+          <>
+          <div>{getTimeWithOffset.props.location}</div>
             <div className= "clock">{this.state.getTimeWithOffset}</div>
+            </>
         );
     }
 }
