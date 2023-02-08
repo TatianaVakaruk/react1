40a11e2e3d02a732c2f90a136c0f9274ca5ebc12
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-08 16:14:22 +0200

    commit1

---
+++ lesson4/task2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Clock from './Clock.jsx';
+
+const App = ()=>{
+    return (
+        <>
+        <Clock location="Kiev" offset="2"/>
+        
+        </>
+    );
+};
+export default App;
---
+++ lesson4/task2/src/Clock.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+const getTimeWithOffset = offset => {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+const clock1 =new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+
+  let hours = clock1.getHours();
+ let  minutes= clock1.getMinutes();
+  let seconds = clock1.getSeconds();
+  
+   let clock;
+  if (hours > 12){
+    if (seconds < 10){
+      seconds = "0" + seconds;
+       }
+      if (minutes < 10){
+        minutes = "0" + minutes;
+       
+      }
+    hours = hours - 12;
+clock = `${hours}:${minutes}:${seconds} P.M.`;
+  }
+  
+  else if (hours === 24){
+    if (seconds < 10){
+        seconds = "0" + seconds;
+       }
+      if (minutes < 10){
+        minutes = "0" + minutes;
+       
+      }
+    clock = `00: ${minutes}:${seconds} A.M.`;
+  }
+  else{
+    if (seconds < 10){
+        seconds = "0" + seconds;
+       }
+      if (minutes < 10){
+        minutes = "0" + minutes;
+       
+      }
+    clock = `${hours}: ${minutes}:${seconds} A.M.`;
+  }
+  return clock;
+  }; 
+
+class Clock extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            getTimeWithOffset:getTimeWithOffset(props.offset)
+        }
+        
+setInterval(()=>{
+    this.setState({
+        getTimeWithOffset:getTimeWithOffset(props.offset)
+    });
+    
+    }, 1000);
+    }
+    
+
+    render (){
+        return (
+            <div className= "clock">{this.state.getTimeWithOffset}</div>
+        );
+    }
+}
+export default Clock;
