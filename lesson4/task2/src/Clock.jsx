33cdd34f4e4aa525ee4f848007200ff9af8b9337
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-08 17:56:04 +0200

    commit1

--- lesson4/task2/src/Clock.jsx
+++ lesson4/task2/src/Clock.jsx
@@ -4,19 +4,18 @@ import moment from 'moment';
 const getTimeWithOffset = offset=> {
     const currentTime = new Date();
     const utcOffset = currentTime.getTimezoneOffset() / 60;
-return moment(new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset))).format('h:mm:ss');
- }; 
-
+return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+}
 class Clock extends Component {
     constructor(props){
         super(props);
         this.state={
-            getTimeWithOffset:getTimeWithOffset(props.offset)
+            getTimeWithOffset:moment(getTimeWithOffset(props.offset)).format('h:mm:ss')
 }
         
 setInterval(()=>{
     this.setState({
-        getTimeWithOffset:getTimeWithOffset(props.offset)
+        getTimeWithOffset:moment(getTimeWithOffset(props.offset)).format('h:mm:ss')
         
     });
     
