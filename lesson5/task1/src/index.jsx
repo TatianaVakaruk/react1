57dd0e15daeb7a820419c780b13c6a5743bc6454
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-10 17:00:57 +0200

    сщььше1

--- lesson5/task1/src/index.jsx
+++ lesson5/task1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import GoodButton from './GoodButton.jsx';
+import GoodButton from './GoodButton';
 
 const rootElement = document.querySelector('#root');
 
