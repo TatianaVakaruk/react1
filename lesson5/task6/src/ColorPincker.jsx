9839661e4a4a15f7fba21631e6a6d78fba3e5553
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-12 20:57:20 +0200

    commit1

---
+++ lesson5/task6/src/ColorPincker.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+const buttonCoral= document.querySelector(".picker__button.picker__button_coral");
+const buttonAqua= document.querySelector(".picker__button.picker__button_aqua");
+const buttonBisque= document.querySelector(".picker__button.picker__button_bisque");
+class ColorPincker extends Component {
+    
+        constructor(props){
+            super(props);
+            this.state={
+                toggle:'',
+               
+            };
+        }
+        setColor(){
+        if (buttonCoral){
+            this.setState({
+                toggle:'Coral',
+        })
+    }
+        if (buttonAqua){
+            this.setState({
+                toggle:'Aqua',
+        })
+        }
+        if (buttonBisque){
+            this.setState({
+                toggle:'Bisque',
+        })
+        }
+    }
+    Clear(){
+        this.setState({
+            toggle:''
+        })
+    }
+    render(){
+        return (
+            <div>
+  <div className="picker__title">{this.state.toggle}</div>
+  <div>
+    <button onMouseEnter={()=>this.setColor()} onMouseLeave={()=>this.Clear()} className="picker__button picker__button_coral"></button>
+    <button onMouseEnter={()=>this.setColor()} onMouseLeave={()=>this.Clear()} className="picker__button picker__button_aqua"></button>
+    <button onMouseEnter={()=>this.setColor()} onMouseLeave={()=>this.Clear()} className="picker__button picker__button_bisque"></button>
+  </div>
+</div>
+        )
+    }
+}
+export default ColorPincker;
---
+++ lesson5/task6/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import ColorPincker from './ColorPincker.jsx';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<ColorPincker />, rootElement);
