01ba408b8ac83cb50c2a750a070b23568322a5ec
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-12 18:00:44 +0200

    commit1

---
+++ lesson5/task5/src/Toggler.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+let counter = 0;
+class Toggler extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            toggle:'Off',
+           
+        };
+    }
+    handleClick (){
+        counter++;
+        console.log(counter);
+        if (counter % 2 === 1){
+        this.setState({
+        toggle:'On',
+        
+    })
+}
+if (counter % 2 === 0){
+    
+    this.setState({
+        toggle:'Off',
+        
+    })
+}
+    }
+    render(){
+        return (
+            <div onClick={()=>{this.handleClick()}} className="toggler">{this.state.toggle}</div>
+        )
+    }
+    
+}
+
+export default Toggler;
