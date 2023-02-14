598865f98ec7c86d9ecb01ef7c32ec0514641cb2
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-14 20:38:01 +0200

    commit1

--- lesson6/task3/src/Page.jsx
+++ lesson6/task3/src/Page.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Info from './Info.jsx';
 const text1 = 'Price is 500$. Available in 2 colors';
-const text2= 'Price is 650$. Not available in 1 color.';
+const text2= 'Price is 650$. Not available in 1 color';
 
 class Page extends Component{
     constructor(props){
