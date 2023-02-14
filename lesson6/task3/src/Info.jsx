6423a73ba78f30fb70d6778e055b91a91509ae52
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-14 20:31:38 +0200

    commit1

---
+++ lesson6/task3/src/Info.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Info=({ text })=>{
+    
+    return (
+        <div className='message'>
+            {text}
+        </div>
+    )
+}
+
+export default Info;
---
+++ lesson6/task3/src/Page.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Info from './Info.jsx';
+const text1 = 'IPhone 13- Price is 500$. Available in 2 colors';
+const text2= 'Price is 650$. Not available.';
+
+class Page extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            text:null
+        }
+    }
+    Info=text=>{
+this.setState({
+    text
+})
+    }
+    render(){
+        return (
+            <div className="page">
+  <Info text={this.state.text}/>
+  <div className="actions">
+    <button  onClick = {()=>this.Info(text1)} className="btn">IPhone 13</button>
+    <button onClick = {()=>this.Info(text2)} className="btn">IPhone 13 Pro</button>
+    <button onClick = {()=>this.Info('')} className="btn">Clear</button>
+  </div>
+</div>
+        )
+    }
+}
+export default Page;
