7113622a5aadc19d97aac953bd2135126cd53623
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-18 01:32:01 +0200

    commit1

--- lesson7/task1/src/index.jsx
+++ lesson7/task1/src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import NumbersList from './NumbersList';
+
 const numbers = [1, 2, 3, 4, 5];
-const numbersElems = numbers.map(num=><li>{num}</li>)
+
 const rootElement = document.querySelector('#root');
-ReactDOM.render( numbersElems, rootElement);
+
+ReactDOM.render(<NumbersList numbers={numbers} />, rootElement);
