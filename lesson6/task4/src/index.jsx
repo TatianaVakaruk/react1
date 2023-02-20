72520e64514d3aeef619c806b04edccdac87cec1
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-20 16:25:35 +0200

    commit1

--- lesson6/task4/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Status from './Status.jsx';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<Status isOnline= {false}/>, rootElement);
