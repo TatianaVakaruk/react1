2a8133555b62a60ced244d7e4e10bf15496ef1d2
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-20 11:07:45 +0200

    сщььше1

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -7,9 +7,12 @@ class Life extends Component{
     }
     componentDidMount(){ console.log('componentDidMount: API calls, subscriptions');
 }
-    shouldComponentUpdate(nextProps, nextState){ console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render'); return nextProps.number % 2;
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+        return nextProps.number % 2;
 }
-    componentDidUpdate(prevProps, prevState){ console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+    componentDidUpdate(prevProps, prevState){
+        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
 }
     componentWillUnmount(){
         console.log('componentWillUnmount(): cleanup before DOM related to component will be removed'); 
