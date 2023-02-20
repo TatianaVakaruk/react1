44255a966a7603965397c66cc061b058b6896ddc
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-20 16:17:45 +0200

    commit1

--- lesson8/task1/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Demo from './Demo';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<Demo />, rootElement);
