4f6e6a25de4d99965519ef6ab469ebe63a4da127
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-20 10:48:18 +0200

    commit1

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -5,8 +5,7 @@ class Life extends Component{
         super(props);
         console.log('constructor: good place to create state');
     }
-    componentDidMount(){
-        console.log('componentDidMount: API calls, subscriptions');}
+    componentDidMount(){console.log('componentDidMount: API calls, subscriptions');}
     shouldComponentUpdate(nextProps, nextState){
         console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
         return nextProps.number % 2;
