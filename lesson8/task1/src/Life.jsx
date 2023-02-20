9bb40629f3bae26f13671bb511b9ae7e15b36c55
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-20 11:37:00 +0200

    commit1

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -10,6 +10,7 @@ class Life extends Component{
     }
     shouldComponentUpdate(nextProps, nextState){
         console.log(' shouldComponentUpdate(nextProps, nextState):decide to render or not to render');
+        return nextProps.number % 2;
     }
     componentDidUpdate(prevProps, prevState){
         console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
