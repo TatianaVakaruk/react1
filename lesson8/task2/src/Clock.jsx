a454edd09de746009f793eb40f4561ba425ba0d6
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-20 18:34:05 +0200

    commit1

--- lesson8/task2/src/Clock.jsx
+++ lesson8/task2/src/Clock.jsx
@@ -4,14 +4,16 @@ class Clock extends Component{
     constructor(props){
         super(props);
         this.state={
-            date: new Date()
+            date: new Date(),
+            location:this.props.location
         }
         
     }
     componentDidMount(){
        this.interval= setInterval(()=>{
             this.setState({
-                date: new Date()
+              date: new Date(),
+              location:this.props.location
             });
         }, 1000)
     }
@@ -19,7 +21,16 @@ class Clock extends Component{
 clearInterval(this.interval);
     }
     render(){
-        return <div>{this.state.date.toLocaleTimeString()}</div>
+        return (
+        <div className="clock">
+  <div className="clock__location">
+{this.state.location}
+  </div>
+  <div className="clock__time">
+    {this.state.date.toLocaleTimeString()}
+  </div>
+</div>
+)
         }
 }
 export default Clock;
