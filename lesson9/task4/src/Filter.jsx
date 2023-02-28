6758cee48df8f3fa0e6215594ac4db5662654e9a
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-02-28 11:20:23 +0200

    commit1

--- lesson9/task4/src/Filter.jsx
+++ lesson9/task4/src/Filter.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
-const Filter =(filterText, count, onChange)=>{
-    let filter;
+const Filter =({filterText, count, onChange})=>{
+   /* let filter;
  onChange=()=>(e)=>{
     filterText = e.target.value.toLowerCase();
 
-filter = this.props.users.filter(user => {
+/*filter = this.props.users.filter(user => {
        return user.name.toLowerCase().includes(filterText);
     
     });
-}
-let isChange=()=>{
-   
-    
-    count = this.props.users.map(user =>{
-      user.name.toLowerCase().includes(filterText);
-        
-    })
-    return count.length;
-    
-}
+}*/
+
 
 
     return(
         <div className="filter">
-    <span className="filter__count">{isChange}</span>
-    <input type="text" onChange={onChange} className="filter__input"  />
+    <span className="filter__count">{count}</span>
+    <input type="text" value ={filterText} onChange={onChange} className="filter__input"  />
   </div>
     )
 
--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -1,37 +1,39 @@
-import React, { Component } from 'react';
-import User from './User';
-import Filter from './Filter';
+import React, { Component } from "react";
+import User from "./User";
+import Filter from "./Filter";
 
 class UsersList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-  this.state={
-    sorting:null,
-    term: '',
-   }
-  }
-  
- dataSearch = () => {
-  let filter;
-  this.props.onChange;
-
- this.setState({
-    sorting: filter,
-   term:value,
-
-  });
-}
-    render(){
+    this.state = {
+      
      
+      //filterUsers:this.props.users,
+      filterText:''
+    };
+  }
 
-        return(
-            <div>
-<Filter value={this.state.term} onChange={this.dataSearch}/>
-  <ul className="users">
-  {this.props.users.map(user => <User key= {user.id} {...user}/>)}
-  </ul>
-  </div>
-        )
-    }
+  dataSearch = (event) => {
+   let filterText= event.target.value.toLowerCase();
+
+    this.setState({
+    //...this.state,
+      filterText,
+      
+    });
+  };
+  render() {
+    let filterUsers= this.props.users.filter(user=>user.name.toLowerCase().includes(this.state.filterText));
+    return (
+      <div>
+        <Filter count ={filterUsers.length} filterText={this.state.filterText} onChange={this.dataSearch} />
+        <ul className="users">
+          {filterUsers.map((user) => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 export default UsersList;
