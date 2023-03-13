66d99f79536cbdc629f63d11e27dc6e70f25825d
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-03-13 06:34:13 +0200

    commit1

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Expand = (props)=>{
-    
+class Expand extends React.Component{ 
+  render(){   
     return(
         <div className="expand border">
         <div className="expand__header">
-          <span className="expand__title">{props.title}</span>
-          <button onClick={() => { this.props.toggleDialog(this.state.isOpen)}} className="expand__toggle-btn">
+          <span className="expand__title">{this.props.title}</span>
+          <button onClick={()=>{ this.props.toggleDialog(this.state.isOpen)}} className="expand__toggle-btn">
             <i className="fas fa-chevron-up"></i>
           </button>
         </div>
-        <div className="expand__content">{props.children}
+        <div className="expand__content">{this.props.children}
         </div>
   </div>
-    )
+    )}
 }
 export default Expand;
