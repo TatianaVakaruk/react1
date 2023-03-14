6914a8a2b4bce3a0b3198ea2d689fa55541ba1a0
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-03-14 09:46:43 +0200

    сщььше1

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -10,7 +10,10 @@ render(){
         <div className="app">
             
         <Expand  title='Some title'>
-       
+        <p>
+        Hooks are a new addition in React 16.8. They let you use state and other React features
+        without writing a class.
+      </p>
         </Expand>
   </div>
     );
--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import Children from './Children';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Expand extends React.Component{ 
-  state={
-    isOpen: false,
-}
+class Expand extends Component {
+  state = {
+    isExpanded: false,
+  };
 
-toggleDialog=()=>{
- if (this.state.isOpen === false){
-  this.setState({
-    isOpen:true
-  })
- }
- else{
-  this.setState({
-    isOpen:false
-  })
- }
-}     
-   
-  render(){  
-    let result =(this.state.isOpen===true && <Children/>);
-    let teg;
-    if (this.state.isOpen){
-      teg= <i className="fas fa-chevron-up"></i>
-    }
-    else {
-      teg=<i className="fas fa-chevron-down"></i>
-    }
-    return(
+  toggleContent = () => {
+    this.setState({
+      isExpanded: !this.state.isExpanded,
+    });
+  };
+
+  render() {
+    const { isExpanded } = this.state;
+    const { title, children } = this.props;
+    return (
       <div className="expand border">
         <div className="expand__header">
-          <span className="expand__title">{this.props.title}</span>
-          <button onClick={this.toggleDialog} className="expand__toggle-btn">
-          {teg}
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleContent}>
+            {isExpanded ? (
+              <i className="fas fa-chevron-up" />
+            ) : (
+              <i className="fas fa-chevron-down" />
+            )}
           </button>
         </div>
-        <div className="expand__content">{result}
-        </div>
-  </div>
-    )}
+        {this.state.isExpanded && <div className="expand__content">{children}</div>}
+      </div>
+    );
+  }
 }
+
+Expand.propTypes = {
+  title: PropTypes.string,
+};
+
+Expand.defaultProps = {
+  title: '',
+};
+
 export default Expand;
